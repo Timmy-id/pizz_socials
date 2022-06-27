6ba9a6b87840a30dd8da0e7f62577a29d0f08e0a
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-06-27 18:31:59 +0100

    add the auth controller and auth routes

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
+import { registerUser, loginUser } from '../Controllers/AuthController.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async(req, res) => {
-    res.send("Auth Route")
-})
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
-export default router
+export default router;
